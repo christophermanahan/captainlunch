9340b0e4f860892d0c768684aaf28c60e49bdde4
Christopher Manahan <chris.a.manahan@gmail.com>
2019-06-27 09:41:28 -0400

    temp2

--- src/test/java/io/github/christophermanahan/captainlunch/web/OverrideCaptainControllerTest.java
+++ src/test/java/io/github/christophermanahan/captainlunch/web/OverrideCaptainControllerTest.java
@@ -1,10 +1,25 @@
 package io.github.christophermanahan.captainlunch.web;
 
+import io.github.christophermanahan.captainlunch.service.ManualRotationService;
+import io.github.christophermanahan.captainlunch.service.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 class OverrideCaptainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ValidationService validationService;
+
+    @MockBean
+    private ManualRotationService rotationService;
+
+    @MockBean
+    private Client client;
 
 }
